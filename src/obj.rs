61a611326bb4fc97040e8ef7a5eeb4a686dac84d
Romain Blazevic <76215581+Zhariel@users.noreply.github.com>
2022-09-28 22:23:21 +0200

    Add files via upload

--- src/obj.rs
+++ src/obj.rs
@@ -5,6 +5,7 @@ use std::str::SplitWhitespace;
 use vek::Vec3;
 
 pub fn extract_obj(filename: String) -> Mesh {
+    // accepts triangulated mesh with normals and texture coordinates
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
@@ -23,12 +24,42 @@ pub fn extract_obj(filename: String) -> Mesh {
                 mesh.vertices.push(Vec3::new(vec[0], vec[1], vec[2]));
             }
             Some("f") => {
-                let indices: Vec<usize> = line.map(|x| x.split('/').next().unwrap().parse().unwrap()).collect();
-                mesh.triangles.push(Triangle {
-                    a: mesh.vertices[indices[0] - 1],
-                    b: mesh.vertices[indices[1] - 1],
-                    c: mesh.vertices[indices[2] - 1],
-                });
+                // let indices: Vec<usize> = line.map(|x| x.split('/').next().unwrap().parse().unwrap()).collect();
+                // mesh.triangles.push(Triangle {
+                //     a: mesh.vertices[indices[0] - 1],
+                //     b: mesh.vertices[indices[1] - 1],
+                //     c: mesh.vertices[indices[2] - 1],
+                // }
+
+                // i32::from_str(&"0").unwrap_or(0);
+                let f_indices: Vec<Vec<&str>> = line.map(|x| x.split('/').collect::<Vec<&str>>()).collect();
+                let triangle = Triangle{
+                    a: f_indices[1][0] as usize,
+                    b: f_indices[1][0] as usize,
+                    c: f_indices[2][0] as usize,
+                };
+                // f_indices.into_iter().map(|x: Vec<&str>| x[0]).collect();
+                print!(" {:?},", f_indices);
+
+                // f_indices.clone().into_iter().for_each(|i| print!(" {:?},", i));
+                // let a = line.split(|c: char| c.is_whitespace() || c == '/').collect::<Vec<&str>>();
+
+                // f_indices.into_iter().for_each(|x| {
+                //
+                // });
+                // let v_indices: Vec<usize> = f_indices.into_iter().split('/').next().unwrap().
+                // v_indices.into_iter().for_each(|i| print!(" {},", i));
+                // for i in 0..indices.len(){
+                //     let vert = indices[i].split('/').next().unwrap();
+                //     let a = 0;
+                // }
+
+                // mesh.triangles.push(Triangle {
+                //     a: indices[0],
+                //     b: indices[1],
+                //     c: indices[2]
+                // });
+                println!();
             }
             _ => {}
         }
